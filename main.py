dbb2f4fdf541657ee9a191f50ff76fb3922e6874
Rss <romeosa@hotmail.gr>
2023-05-03 15:00:29 +0300

    Nothing

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ class App(customtkinter.CTk):
         self.tabview.add("Homepage")
         self.tabview.add("Timetable")
         self.tabview.add("Daily Manager")
-        
-        #-----------------------------------------------------------#
+
+        # -----------------------------------------------------------#
         # Create custom grid for homepage tab
         self.homepage_frame = self.tabview.tab("Homepage")
 
@@ -89,8 +89,8 @@ class App(customtkinter.CTk):
             self.homepage_frame, text="News", fg_color='green')
         self.right_lower.grid(row=1, column=1, rowspan=10,
                               padx=10, pady=10, sticky="nsew")
-        
-        #-----------------------------------------------------------#
+
+        # -----------------------------------------------------------#
         # Timetable Tab
         self.Timetable_frame = self.tabview.tab("Timetable")
         self.Timetable_frame.columnconfigure(0, weight=1)
@@ -106,29 +106,28 @@ class App(customtkinter.CTk):
         self.Timetable_frame.rowconfigure(8, weight=2)
         self.Timetable_frame.rowconfigure(9, weight=2)
         self.Timetable_frame.rowconfigure(10, weight=2)
-        
+
         # Widgets for Timetable tab
         # Top / Scheduling and management of meetings
         self.left_upper = customtkinter.CTkButton(
             self.Timetable_frame, text="Scheduling and management of meetings", fg_color='#4F02FF', cursor="hand2")
         self.left_upper.grid(row=0, column=0, rowspan=3, columnspan=3,
                              padx=10, pady=10, sticky="nsew")
-        
+
         # Center / Calls and send text messages SMS
         self.left_center = customtkinter.CTkButton(
             self.Timetable_frame, text="Calls and send text messages SMS", fg_color='green', cursor="hand2")
         self.left_center.grid(row=3, column=0, rowspan=3, columnspan=2,
-                             padx=10, pady=10, sticky="nsew")
-        
+                              padx=10, pady=10, sticky="nsew")
+
         # Bottom / Organise and manage contacts
         self.left_lower = customtkinter.CTkButton(
             self.Timetable_frame, text="Organise and manage contacts", fg_color='#72BFF4', cursor="hand2")
         self.left_lower.grid(row=6, column=0, rowspan=5, columnspan=2,
                              padx=10, pady=10, sticky="nsew")
-        
-        
-        #-----------------------------------------------------------#
-        #Daily Manager
+
+        # -----------------------------------------------------------#
+        # Daily Manager
         self.dManager_frame = self.tabview.tab("Daily Manager")
         self.dManager_frame.columnconfigure(0, weight=1)
         self.dManager_frame.columnconfigure(1, weight=2)
@@ -153,32 +152,33 @@ class App(customtkinter.CTk):
             self.dManager_frame, text="Task Manager", fg_color='dark red', cursor="hand2")
         self.left_upper.grid(row=0, column=0, rowspan=3, columnspan=3,
                              padx=10, pady=10, sticky="nsew")
-        
+
         # Left center / Navigator
         self.left_center = customtkinter.CTkButton(
             self.dManager_frame, text="Navigator", fg_color='green', cursor="hand2")
         self.left_center.grid(row=3, column=0, rowspan=3, columnspan=2,
-                             padx=10, pady=10, sticky="nsew")
-        
+                              padx=10, pady=10, sticky="nsew")
+
         # Left lower / Device controler
         self.left_lower = customtkinter.CTkButton(
             self.dManager_frame, text="Device Controler", fg_color='blue', cursor="hand2")
         self.left_lower.grid(row=6, column=0, rowspan=5, columnspan=2,
                              padx=10, pady=10, sticky="nsew")
-        
+
         # Right upper / Reminder
         self.right_upper = customtkinter.CTkButton(
             self.dManager_frame, text="Reminders & Alarms", fg_color='coral', cursor="hand2")
         self.right_upper.grid(row=0, column=3, rowspan=3, columnspan=2,
                               padx=10, pady=10, sticky="nsew")
-        
-         # Right lower / E-shop
+
+        # Right lower / E-shop
         self.right_lower = customtkinter.CTkButton(
             self.dManager_frame, text="Your shop", fg_color='light green', cursor="hand2")
         self.right_lower.grid(row=3, column=2, rowspan=8, columnspan=3,
                               padx=10, pady=10, sticky="nsew")
-        #-----------------------------------------------------------#
-        
+        # -----------------------------------------------------------#
+
+
 if __name__ == "__main__":
     app = App()
     app.mainloop()
