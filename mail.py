123a618040b91ff265c48c18f274f8bdc5078e3b
panagiotis livanis <panagiotislivanis@gmail.com>
2023-06-28 20:21:16 +0300

    bug fix +mail

---
+++ mail.py
@@ -0,0 +1,84 @@
+import customtkinter as ctk
+import yagmail
+import smtplib
+import ssl
+from email.message import EmailMessage
+from CTkMessagebox import CTkMessagebox
+
+# Modes: "System" (standard), "Dark", "Light"
+ctk.set_appearance_mode("dark")
+# Themes: "blue" (standard), "green", "dark-blue"
+ctk.set_default_color_theme("blue")
+
+SCOPES = ['https://www.googleapis.com/auth/gmail.send']
+SERVICE_ACCOUNT_FILE = 'key.json'
+
+class EmailApp(ctk.CTkFrame):
+    def show_checkmark(self):
+        # Show some positive message with the checkmark icon
+        CTkMessagebox(message="success",
+                      icon="check", option_1="Thanks")
+
+    def show_error(self, error_message):
+        # Show some error message
+        CTkMessagebox(title="Error", message='Error', icon="cancel")
+    
+    def __init__(self, parent):
+        super().__init__(parent, bg_color="white")
+        self.parent = parent
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.label_to = ctk.CTkLabel(self, text="To:")
+        self.label_to.pack()
+
+        self.entry_to = ctk.CTkEntry(self, width=450)
+        self.entry_to.pack()
+
+        self.label_subject = ctk.CTkLabel(self, text="Subject:")
+        self.label_subject.pack()
+
+        self.entry_subject = ctk.CTkEntry(self, width=450)
+        self.entry_subject.pack()
+
+        self.label_message = ctk.CTkLabel(self, text="Message:")
+        self.label_message.pack()
+
+        self.entry_message = ctk.CTkEntry(self, width=450, height=450)
+        self.entry_message.pack()
+
+        self.button_send = ctk.CTkButton(
+            self, text="Send", command=self.send_email)
+        self.button_send.pack()
+
+    def send_email(self):
+        receiver = self.entry_to.get()
+        subject = self.entry_subject.get()
+        message = self.entry_message.get()
+        port = 465  # For SSL
+        smtp_server = "smtp.gmail.com"
+        sender_email = "ithvanithvan@gmail.com"
+        password = "htrrapepazexbzlx"
+
+        msg = EmailMessage()
+        msg.set_content(message)
+        msg['Subject'] = subject
+        msg['From'] = sender_email
+        msg['To'] = receiver
+        context = ssl.create_default_context()
+
+        try:
+            with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+                server.login(sender_email, password)
+                server.send_message(msg, from_addr=sender_email, to_addrs=receiver)
+                self.show_checkmark()
+        except Exception as e:
+            self.show_error(str(e))
+            
+if __name__ == "__main__":
+    app = ctk.CTk()
+    app.title("Email App")
+    app.geometry("400x400")
+    email_app = EmailApp(app)
+    email_app.pack(fill="both", expand=True)
+    app.mainloop()
--- traffic.py
+++ traffic.py
@@ -1,111 +1,42 @@
-import requests
-import folium
-import requests
-from geopy.geocoders import Nominatim
-from geopy.exc import GeocoderTimedOut, ConfigurationError
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from PIL import Image
-import os
-import threading
 import customtkinter as ctk
-from whether import create_whether
+from traffic_map import make_map
+
+
+
+
 # Modes: "System" (standard), "Dark", "Light"
 ctk.set_appearance_mode("dark")
 # Themes: "blue" (standard), "green", "dark-blue"
 ctk.set_default_color_theme("blue")
 
 
-class traffic_widget(ctk.CTkFrame):
 
+class TrafficWidget(ctk.CTkFrame):
+    make_map()
     def __init__(self, parent, bg_color="transparent"):
         super().__init__(parent, bg_color=bg_color)
         self.create_widget()
 
     def create_widget(self):
         photo = ctk.CTkImage(Image.open(
-            "images/final.png"), size=(2000, 1400))
+            "images/final_2.png"), size=(2000, 1200))
         image_label = ctk.CTkLabel(
             self, text='', image=photo, padx=0, pady=0)
         image_label.image = photo
         image_label.pack(side="top", pady=(0, 0))
 
 
-city = "θεσσαλονικη"
+  # ftp.retrbinary(filename_ftp, open(filename_local, "wb").write)
+ # ftp.quit()
 
 
-def get_traffic_data(city):
-
-    geolocator = Nominatim(user_agent="ithvan")
-    location = geolocator.geocode(city)
-    if location:
-        latitude = location.latitude
-        longitude = location.longitude
-        return latitude, longitude
-    else:
-        print("Δεν βρέθηκαν συντεταγμένες για την πόλη.")
-
-    if location is not None:
-        response = requests.get(
-            "https://api.tomtom.com/traffic/services/4/flowSegmentData/absolute/10/json",
-            params={
-                "point": f"{latitude},{longitude}",
-                "unit": "MPH",
-                "key": "eRuAxCtEML4iTUX0urNXWgHOd0hM5CbO"
-            }
-        )
-    else:
-        print("Δεν ήταν δυνατό να βρεθεί η τοποθεσία σας.")
-    return latitude, longitude
-
-
-def create_traffic_map(latitude, longitude):
-    m = folium.Map(location=[latitude, longitude],
-                   zoom_start=12, tiles="OpenStreetMap")
-
-    data = get_traffic_data(city)
-    m.save("images/traffic_map.html")
-
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    driver = webdriver.Chrome(options=chrome_options)
-
-    driver.get("file://" + os.path.abspath("images/traffic_map.html"))
-    driver.set_window_size(1920, 1080)
-
-    driver.save_screenshot("images/traffic_map_screenshot.png")
-    driver.quit()
-
-    image = Image.open("images/traffic_map_screenshot.png")
-    image = image.crop((560, 200, image.width - 550, image.height - 200))
-    # image = image.resize(1920, 1080)
-    # Αποθήκευση της εικόνας σε μορφή JPEG
-    print("Saving image as PNG...")
-    image.save("images/traffic_map.png", "PNG")
-    print("Image saved as traffic_map.PNG")
-    create_whether()
-    whether = Image.open("images/whether.png")
-    back_im = image.copy()
-    image.paste(whether, (400, 0))
-    image.save('images/final.png', quality=95)
-    path = os.path.abspath("images/traffic_map.png")
-    return path
-
-
-def create_traffic_map_repeat():
-    create_traffic_map()
-    # Επανάληψη της δημιουργίας του χάρτη κάθε 5 λεπτά (300 δευτερόλεπτα)
-    threading.Timer(30, create_traffic_map_repeat).start()
-
-
-latitude, longitude = get_traffic_data(city)
-create_traffic_map(latitude, longitude)
+    
 if __name__ == "__main__":
     app = ctk.CTk()
     app.title("test")
     app.geometry("400x660")
-    widget = traffic_widget(app)
+    widget = TrafficWidget(app)
     widget.pack(fill="both", expand=True)
     app.mainloop()
 
-# create_traffic_map_repeat()
